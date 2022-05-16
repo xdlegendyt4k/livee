aebbef9e1cafa0e99534bdf062bc9fe38f807b2e
ggkk77121 <93269061+ggkk77121@users.noreply.github.com>
2022-05-16 10:45:57 +0530

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,1 @@
+ffmpeg -stream_loop -1 -re -i ok.mp4 -stream_loop -1 -re -i http://stream.zeno.fm/v4wf5ezpxrhvv -vcodec libx264 -pix_fmt yuvj420p -maxrate 20048k -preset veryfast -r 12 -framerate 30 -g 50 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150000k -f flv  rtmp://a.rtmp.youtube.com/live2/xr2e-pjfq-b0gu-tk7z-22pg
